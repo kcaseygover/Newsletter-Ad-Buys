c3679b6f71e66276906317a82bb1932fdcbe77a5
Karen Casey-Gover <karen@manifold.co>
2017-05-16 10:14:56 -0300

    Populate publication select menu with data

--- src/components/Publication.js
+++ src/components/Publication.js
@@ -4,46 +4,7 @@ import PropTypes from 'prop-types';
 import data from '../../data';
 
 const Publication = (props) => {
-
-  let key;
-  let key2;
-  function findPublication (obj) {
-    for (key in obj) {
-      console.log(Object.keys(obj[key]))
-      let i;
-      for (i = 0; i < Object.keys(obj[key]).length; i++) {
-        if (Object.keys(obj[key])[i] === props.publisher) {
-          console.log('publisher matches')
-          console.log(Object.keys(obj[key])[i])
-          console.log(obj[key])
-          for (key2 in obj[key]) {
-            console.log(Object.keys(obj[key][key2].Publication))
-          }
-          } else {
-            console.log('try again')
-          }
-        }
-      }
-    }
-findPublication(data);
-
-  const publishers = Object.keys(data.Publishers);
-
-  console.log('in publication', 'props.publisher', props.publisher, 'data.publishers', publishers[0] )
-  const phpPublications = Object.keys(data.Publishers['PHP Weekly'].Publication);
-  const mappedPhpPublications = phpPublications.map(publication =>
-    <option key={publication.toString()}>{publication}</option>
-  )
-
-  const laravelNewsPublications = Object.keys(data.Publishers['Laravel News'].Publication);
-  const mappedLaravelNewsPublications = laravelNewsPublications.map(publication =>
-    <option key={publication.toString()}>{publication}</option>
-  )
-
-  const cooperpressPublications = Object.keys(data.Publishers['Cooperpress'].Publication);
-  const mappedCooperpressPublications = cooperpressPublications.map(publication =>
-    <option key={publication.toString()}>{publication}</option>
-  )
+  const publisher = props.publisher;
 
   return (
     <div>
@@ -53,11 +14,10 @@ findPublication(data);
         onChange={props.onChangePublication}
         >
         <option defaultValue >Select...</option>
-        { props.publisher === publishers[1] ?
-          mappedPhpPublications ||
-          mappedLaravelNewsPublications ||
-          mappedCooperpressPublications :
-          <option></option>
+        { publisher ?
+          (Object.keys(data.Publishers[publisher].Publication)).map(publication =>
+          <option key={publication.toString()}>{publication}</option>
+          ) : null
         }
       </select>
     </div>
